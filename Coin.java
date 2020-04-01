48433b87653acec574c28420c3771ed72e93fdeb
priyapradeep19 <51538983+priyapradeep19@users.noreply.github.com>
2020-04-01 11:11:30 -0700

    Add files via upload

---
+++ Coin.java
@@ -0,0 +1,32 @@
+
+/*Priya Pradeep
+35A ONLINE	
+Assignment Number 1	
+October 5
+10/19/18
+*/
+import java.util.Random;
+
+public class Coin {
+	private String sideUp;
+
+	public void toss() {
+		Random rand = new Random();
+		int randNumber;
+
+		randNumber = rand.nextInt(2);
+		if (randNumber == 0) {
+			sideUp = "TAILS";
+		} else {
+			sideUp = "HEADS";
+		}
+	}
+
+	public String getSideUp() {
+		return sideUp;
+	}
+
+	public Coin() {
+		toss();
+	}
+}
---
+++ CoinTest.java
@@ -0,0 +1,30 @@
+/*Priya Pradeep
+35A ONLINE	
+Assignment Number 1	
+October 5
+10/19/18
+*/
+
+public class CoinTest {
+	public static void main(String[] args) {
+		Coin coin = new Coin();
+		int numberOfHeads = 0;
+		int numberOfTails = 0;
+		int numberOfTosses = 20;
+
+		System.out.println("INITIAL SIDE: " + coin.getSideUp() + "\n");
+
+		for (int currentToss = 1; currentToss <= 20; currentToss++) {
+			coin.toss();
+
+			if (coin.getSideUp() == "HEADS") {
+				numberOfHeads += 1;
+			} else {
+				numberOfTails += 1;
+			}
+			System.out.println("Toss " + currentToss + ": " + coin.getSideUp());
+		}
+		System.out.println("\n" + "OUT OF " + numberOfTosses + " TOSSES, THERE WERE " + numberOfHeads + " HEADS AND  "
+				+ numberOfTails + " TAILS.");
+	}
+}
